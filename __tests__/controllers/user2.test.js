2b21f4a772a077df35c3aee9f5709259bf498572
Mouad Aoulad Hammoud <mouad.aoulad.hammoud@gmail.com>
2024-01-15 23:31:28 +0100

    Test / API users

--- __tests__/controllers/user2.test.js
+++ __tests__/controllers/user2.test.js
@@ -2,7 +2,13 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("API Users Test", () => {
-  it("GET /api/users should return a list of users", async () => {
+  let server;
+  beforeAll(() => {
+    // Avant que les tests ne commencent, démarrez le serveur (ou initialisez d'autres ressources)
+    server = app.listen(3000); // Remplacez 3000 par le port que votre application utilise
+  });
+
+  it("GET /api/users/all should return a list of users", async () => {
     const response = await request(app)
       .get("/api/users/all")
       .query({ search: "" });
@@ -11,5 +17,10 @@ describe("API Users Test", () => {
     expect(response.body.totalPages).toBeGreaterThan(0);
     expect(response.body.users.length).toBeGreaterThan(0);
     expect(response.body).toHaveProperty("totalPages");
+  });
+
+  afterAll((done) => {
+    // Après que tous les tests soient terminés, arrêtez le serveur (ou nettoyez d'autres ressources)
+    server.close(done);
   });
 });
